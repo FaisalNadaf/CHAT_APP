bfe669d8376231595a30244f713437e49964ee62
faisal <faisalfnadaf@gmail.com>
2024-03-03 21:37:01 +0530

    jwt setup

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,48 +1,64 @@
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 import User from "../models/userModel.js";
+import generateTokenAndCookie from "../utils/generateToken.js";
+
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (!username) {
+      return res.status(400).json({ error: "Username is required" });
+    }
+
     if (password !== confirmPassword) {
       return res.status(400).json({ error: "Password don't match" });
     }
 
-    const user = await User.findOne({ username });
+    // Check if the username already exists
+    const existingUser = await User.findOne({
+      username,
+    });
 
-    if (user) {
-      return res.status(400).json({ error: "Username alredy exists" });
+    if (existingUser) {
+      return res.status(400).json({ error: "Username already exists" });
     }
-    //hash password
 
-    const salt =await bcrypt.genSalt(5);
-    const hashPassword=await bcrypt.hash(password,salt);
-    //avator place holder
+    // Hash password
+    const salt = await bcrypt.genSalt(10); // Increased rounds for better security
+    const hashPassword = await bcrypt.hash(password, salt);
 
+    // Avatar placeholder
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-    const girlProfilePic =  `https://avatar.iran.liara.run/public/girl?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
+    // Create a new user instance
     const newUser = new User({
       fullName,
       username,
-      password:hashPassword,
+      password: hashPassword,
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
 
-    await newUser.save();
+    // Save the new user
+    if (newUser) {
+      await newUser.save();
+      generateTokenAndCookie(newUser._id, res);
 
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      profilePic: newUser.profilePic,
-    });
-    console.log("sinup");
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+
+      console.log("signup successful");
+    }
   } catch (error) {
-    console.log("error in signup controller ",error.message);
+    
+    console.error("Error in signup controller: ", error);
     res.status(500).json({ error: "Server Error" });
   }
-
 };
 
 export const login = (req, res) => {
